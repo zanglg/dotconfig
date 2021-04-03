68c6f5e6ae33bb219e17991243ab5169443c8702
Zang Leigang <zanglg@users.noreply.github.com>
2021-04-03 14:50:30 +0800

    Create completion.lua

---
+++ nvim/lua/completion.lua
@@ -0,0 +1,21 @@
+vim.o.completeopt = "menuone,noselect"
+
+require "lspconfig".clangd.setup {}
+require "lspconfig".pyls.setup {}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+require "lspconfig".rust_analyzer.setup {capabilities = capabilities}
+
+require "compe".setup {
+    source = {
+        path = true,
+        buffer = true,
+        nvim_lsp = true,
+        nvim_lua = true,
+        vsnip = true,
+        emoji = true
+    }
+}
+
+vim.cmd("inoremap <silent><expr> <C-j> compe#confirm('<CR>')")
