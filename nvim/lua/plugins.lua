611e137233e15211faba309281d8537439005fd1
Zang Leigang <zanglg@users.noreply.github.com>
2021-04-06 22:14:36 +0800

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,66 +1,62 @@
-
-local fn  = vim.fn
+local fn = vim.fn
 local cmd = vim.cmd
 
 -- Auto download packer.nvim
 local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
 
 if not packer_exists then
-    local install_path = fn.stdpath('config').."/pack/packer/opt/packer.nvim"
-    cmd('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    local install_path = fn.stdpath("config") .. "/pack/packer/opt/packer.nvim"
+    cmd("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
 
-cmd('packadd packer.nvim')
+cmd("packadd packer.nvim")
 -- Auto compile when there are changes in plugins.lua
-cmd('autocmd BufWritePost plugins.lua PackerCompile')
+cmd("autocmd BufWritePost plugins.lua PackerCompile")
 
-local packer = require('packer')
-local util   = require('packer.util')
-packer.init({
-    package_root = util.join_paths(fn.stdpath('config'), 'pack'),
-    compile_path = util.join_paths(fn.stdpath('config'), 'pack', 'packer_compiled.vim')
-})
-
-packer.startup(function()
-    use {'wbthomason/packer.nvim', opt = true}
-
--- colorscheme and semantic highlight
-    use {'zanglg/nova.vim'}
-    use {'sheerun/vim-polyglot'}
-    use {'glepnir/galaxyline.nvim',
-        branch = 'main',
-        config = "require('statusline')",
-        requires = {'kyazdani42/nvim-web-devicons', opt = true}
+local packer = require("packer")
+local util = require("packer.util")
+packer.init(
+    {
+        package_root = util.join_paths(fn.stdpath("config"), "pack"),
+        compile_path = util.join_paths(fn.stdpath("config"), "pack", "packer_compiled.vim")
     }
+)
 
--- enhanced text edit
-    use {'editorconfig/editorconfig-vim'}
-    use {'junegunn/vim-easy-align'}
-    use {'terryma/vim-multiple-cursors'}
-    use {'rust-lang/rust.vim', ft = {'rust'}}
-    use {'sbdchd/neoformat'}
-    use {'fidian/hexmode'}
-    use {'jiangmiao/auto-pairs'}
+packer.startup(
+    function()
+        use {"wbthomason/packer.nvim", opt = true}
 
--- git related
-    use {'airblade/vim-gitgutter'}
-    use {'tpope/vim-fugitive'}
+        -- colorscheme and semantic highlight
+        use {"zanglg/nova.vim"}
+        use {"sheerun/vim-polyglot"}
+        use {"glepnir/galaxyline.nvim"}
+        use {"kyazdani42/nvim-web-devicons"}
 
--- completion
-    use {'hrsh7th/nvim-compe',
-        config = "require('completion')",
-        requires = {
-            {'neovim/nvim-lspconfig'},
-            {"rafamadriz/friendly-snippets"},
-            {'hrsh7th/vim-vsnip'},
-        }
-    }
+        -- enhanced text edit
+        use {"editorconfig/editorconfig-vim"}
+        use {"junegunn/vim-easy-align"}
+        use {"terryma/vim-multiple-cursors"}
+        use {"rust-lang/rust.vim", ft = {"rust"}}
+        use {"sbdchd/neoformat"}
+        use {"fidian/hexmode"}
+        use {"jiangmiao/auto-pairs"}
 
--- utility
-    use {'junegunn/fzf.vim'}
-    use {'voldikss/vim-floaterm'}
+        -- git related
+        use {"airblade/vim-gitgutter"}
+        use {"tpope/vim-fugitive"}
 
-end)
- 
+        -- completion
+        use {"hrsh7th/nvim-compe"}
+        use {"neovim/nvim-lspconfig"}
+        use {"rafamadriz/friendly-snippets"}
+        use {"hrsh7th/vim-vsnip"}
+
+        -- utility
+        use {"voldikss/vim-floaterm"}
+    end
+)
+
 -- auto download plugins when first download packer.nvim
-if not packer_exists then cmd('PackerSync') end
+if not packer_exists then
+    cmd("PackerSync")
+end
