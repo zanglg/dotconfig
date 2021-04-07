516985fa84849e84d3111c6aa8594020c65f9176
Zang Leigang <zanglg@users.noreply.github.com>
2021-04-07 12:06:28 +0800

    Update completion.lua

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -1,11 +1,12 @@
 vim.o.completeopt = "menuone,noselect"
 
-require "lspconfig".clangd.setup {}
-require "lspconfig".pyls.setup {}
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require "lspconfig".clangd.setup {capabilities = capabilities}
+require "lspconfig".pyls.setup {capabilities = capabilities}
 require "lspconfig".rust_analyzer.setup {capabilities = capabilities}
+require "lspkind".init {}
 
 require "compe".setup {
     source = {
@@ -18,12 +19,32 @@ require "compe".setup {
     }
 }
 
-local snippet = vim.fn.stdpath('config')..'/snippet'
+local snippet = vim.fn.stdpath("config") .. "/snippet"
 vim.g.vsnip_snippet_dir = snippet
 
-vim.api.nvim_set_keymap('i', '<C-j>', "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<C-j>'", {expr = true, noremap = false})
-vim.api.nvim_set_keymap('s', '<C-j>', "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<C-j>'", {expr = true, noremap = false})
-vim.api.nvim_set_keymap('i', '<C-k>', "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-prev)' : '<C-k>'", {expr = true, noremap = false})
-vim.api.nvim_set_keymap('s', '<C-k>', "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-prev)' : '<C-k>'", {expr = true, noremap = false})
+vim.api.nvim_set_keymap(
+    "i",
+    "<C-j>",
+    "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<C-j>'",
+    {expr = true, noremap = false}
+)
+vim.api.nvim_set_keymap(
+    "s",
+    "<C-j>",
+    "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<C-j>'",
+    {expr = true, noremap = false}
+)
+vim.api.nvim_set_keymap(
+    "i",
+    "<C-k>",
+    "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-prev)' : '<C-k>'",
+    {expr = true, noremap = false}
+)
+vim.api.nvim_set_keymap(
+    "s",
+    "<C-k>",
+    "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-prev)' : '<C-k>'",
+    {expr = true, noremap = false}
+)
 
-vim.api.nvim_set_keymap('i', '<C-i>', "compe#confirm('<C-i>')", {expr = true, noremap = true})
+vim.api.nvim_set_keymap("i", "<C-i>", "compe#confirm('<C-i>')", {expr = true, noremap = true})
