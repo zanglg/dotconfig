22243d9c2dd4668337aa8c63f06e4fd6d99afc0f
Zang Leigang <zanglg@users.noreply.github.com>
2021-04-10 15:03:56 +0800

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,69 +1,98 @@
-local fn = vim.fn
-local cmd = vim.cmd
+core.plugins = {
+    completion = require("completion"),
+    statusline = require("statusline"),
+    telescopes = require("telescopes")
+}
 
--- Auto download packer.nvim
-local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
+local plugins = core.plugins
 
-if not packer_exists then
-    local install_path = fn.stdpath("config") .. "/pack/packer/opt/packer.nvim"
-    cmd("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-end
+function plugins:load_packer()
+    if not self.packer then
+        vim.api.nvim_command("packadd packer.nvim")
+        self.packer = require("packer")
 
-cmd("packadd packer.nvim")
--- Auto compile when there are changes in plugins.lua
-cmd("autocmd BufWritePost plugins.lua PackerCompile")
+        self.packer.init({
+            package_root = self.package_root,
+            compile_path = self.compile_path,
+            git = {clone_timeout = 600}
+        })
 
-local packer = require("packer")
-local util = require("packer.util")
-packer.init(
-    {
-        package_root = util.join_paths(fn.stdpath("config"), "pack"),
-        compile_path = util.join_paths(fn.stdpath("config"), "pack", "packer_compiled.vim")
-    }
-)
+        self.packer.reset()
 
-packer.startup(
-    function()
-        use {"wbthomason/packer.nvim", opt = true}
+        self.packer.startup(function()
+            use {"wbthomason/packer.nvim", opt = true}
 
-        -- colorscheme and semantic highlight
-        use {"zanglg/nova.vim"}
-        use {"glepnir/galaxyline.nvim"}
-        use {"kyazdani42/nvim-web-devicons"}
-
-        -- enhanced text edit
-        use {"editorconfig/editorconfig-vim"}
-        use {"fidian/hexmode"}
-
-        -- language specific
-        use {"rust-lang/rust.vim", ft = {"rust"}}
-
-        -- git related
-        use {"airblade/vim-gitgutter"}
-        use {"tpope/vim-fugitive"}
-
-        -- completion
-        use {"neovim/nvim-lspconfig"}
-        use {"hrsh7th/nvim-compe"}
-        use {"onsails/lspkind-nvim"}
-        use {"hrsh7th/vim-vsnip"}
-        use {"rafamadriz/friendly-snippets"}
-
-        -- navigation
-        use {
-            "nvim-telescope/telescope.nvim",
-            requires = {
-                {"nvim-lua/popup.nvim"},
-                {"nvim-lua/plenary.nvim"}
+            -- colorscheme and semantic highlight
+            use {
+                "glepnir/galaxyline.nvim",
+                branch = "main",
+                config = self.statusline.setup,
+                requires = {"kyazdani42/nvim-web-devicons"}
             }
-        }
 
-        -- utility
-        use {"voldikss/vim-floaterm"}
+            -- enhanced text edit
+            use {"editorconfig/editorconfig-vim"}
+            use {"fidian/hexmode", ft = {"bin", "img"}}
+
+            -- language specific
+            use {"rust-lang/rust.vim", ft = {"rust"}}
+
+            -- git related
+            use {"airblade/vim-gitgutter"}
+            use {"tpope/vim-fugitive"}
+
+            -- lspconfig
+            use {
+                "neovim/nvim-lspconfig",
+                ft = {"rust", "c", "cpp", "python"},
+                config = self.completion.lspconfig
+            }
+
+            -- completion
+            use {
+                "hrsh7th/nvim-compe",
+                config = self.completion.compe,
+                requires = {
+                    {"hrsh7th/vim-vsnip"}, {"onsails/lspkind-nvim"},
+                    {"rafamadriz/friendly-snippets"}
+                }
+            }
+
+            -- navigation
+            use {
+                "nvim-telescope/telescope.nvim",
+                cmd = "Telescope",
+                config = self.telescopes.setup,
+                requires = {
+                    {"nvim-lua/popup.nvim", opt = true},
+                    {"nvim-lua/plenary.nvim", opt = true}
+                }
+            }
+
+            -- utility
+            use {"voldikss/vim-floaterm", cmd = "FloatermNew"}
+        end)
     end
-)
-
--- auto download plugins when first download packer.nvim
-if not packer_exists then
-    cmd("PackerSync")
 end
+
+function plugins:sync()
+    self:load_packer()
+    self.packer.sync()
+end
+
+function plugins:init()
+    local config_path = vim.fn.stdpath("config")
+
+    self.install_path = config_path .. "/pack/packer/opt/packer.nvim"
+    self.package_root = config_path .. "/pack"
+    self.compile_path = config_path .. "/plugin/packer_compiled.vim"
+
+    if not vim.loop.fs_stat(self.install_path) then
+        vim.api.nvim_command(
+            "!git clone https://github.com/wbthomason/packer.nvim " ..
+                self.install_path)
+        self:sync()
+    end
+end
+
+return plugins
