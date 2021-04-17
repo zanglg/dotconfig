e8dc413fb2df5d62712bf004e0127d50b2e4e906
Zang Leigang <zanglg@users.noreply.github.com>
2021-04-17 15:59:26 +0800

    Update mappings.lua

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,6 +6,9 @@ function mappings.init()
 
     map("i", "<C-h>", "<C-Left>",  {expr = false, noremap = true})
     map("i", "<C-l>", "<C-Right>", {expr = false, noremap = true})
+    
+    map("n", "<leader>f", "<cmd>Telescope fd<cr>",         {expr = false, noremap = true})
+    map("n", "<leader>/", "<cmd>Telescope live_grep<cr>",  {expr = false, noremap = true})
 end
 
 return mappings
