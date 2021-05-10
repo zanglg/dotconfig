c048f63fd72bc4b6335a3dfba40e05295d731faa
Zang Leigang <zanglg@users.noreply.github.com>
2021-05-10 15:24:11 +0800

    Update statusline.lua

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -5,22 +5,7 @@ function statusline.setup()
     local condition = require("galaxyline.condition")
     local gls = gl.section
 
-    local colors = {
-        foreground = "#364349",
-        inconspicuous = "#6c8693",
-        selection = "#d3dbde",
-        line = "#f0f3f4",
-        background = "#fcfdfd",
-        blue = "#2196f3",
-        green = "#50a14f",
-        purple = "#6666cc",
-        red = "#e45649",
-        pink = "#d81b60",
-        cyan = "#2aa1ae",
-        violet = "#a626a4",
-        yellow = "#C18401",
-        brown = "#8d6e63"
-    }
+    local colors = require('nova').palette.dark
 
     gls.left[1] = {
         FirstElement = {
