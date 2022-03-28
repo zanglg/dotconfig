6fef2702ad34df4badb65d53130fb60d68b36439
Zang Leigang <zang.lg@outlook.com>
2022-03-28 22:20:54 +0800

    refact neovim configs, not finished

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,60 +1,78 @@
-local options = {}
+-- short command and function
+local g         = vim.g
+local o         = vim.opt
 
-function options:init()
-    -- short command and function
-    local o         = vim.opt
+-- file editing
+o.backup        = false                                -- no backup file
+o.swapfile      = false                                -- no swap file
+o.shadafile     = 'NONE'                               -- shadont
+o.undofile      = false                                -- no undo file
+o.encoding      = 'utf-8'                              -- default utf-8
+o.fileformats   = 'unix,mac,dos'                       -- file format
+o.fileencoding  = 'utf-8'                              -- default file encoding
+o.fileencodings = 'ucs-bom,utf-8,cp936,gb18030,latin1' -- file encoding fallback sequence
+o.autoread      = true                                 -- detect when a file is changed
+o.clipboard     = 'unnamed'                            -- yank to system clipboard
+o.backspace     = 'eol,start,indent'                   -- endofline, old char, cross indent
+o.whichwrap     = '<,>,h,l,[,]'                        -- keys can move across line
+o.hidden        = true                                 -- buffer switch without saving
+o.formatoptions = 'tcqmMj'                             -- wrap for multiple byte characters
 
-    -- file editing
-    o.backup        = false                                -- no backup file
-    o.swapfile      = false                                -- no swap file
-    o.shadafile     = 'NONE'                               -- shadont
-    o.undofile      = false                                -- no undo file
-    o.encoding      = 'utf-8'                              -- default utf-8
-    o.fileformats   = 'unix,mac,dos'                       -- file format
-    o.fileencoding  = 'utf-8'                              -- default file encoding
-    o.fileencodings = 'ucs-bom,utf-8,cp936,gb18030,latin1' -- file encoding fallback sequence
-    o.autoread      = true                                 -- detect when a file is changed
-    o.clipboard     = 'unnamed'                            -- yank to system clipboard
-    o.backspace     = 'eol,start,indent'                   -- endofline, old char, cross indent
-    o.whichwrap     = '<,>,h,l,[,]'                        -- keys can move across line
-    o.hidden        = true                                 -- buffer switch without saving
-    o.formatoptions = 'tcqmMj'                             -- wrap for multiple byte characters
+-- search related
+o.hlsearch      = true                                 -- highlight search results
+o.ignorecase    = true                                 -- case insensitive searching
+o.incsearch     = true                                 -- set incremental search, like modern browsers
+o.smartcase     = true                                 -- case-sensitive if expresson contains a capital letter
+o.lazyredraw    = false                                -- don't redraw while executing macros
+o.magic         = true                                 -- Set magic on, comfortable for regex
 
-    -- search related
-    o.hlsearch      = true                                 -- highlight search results
-    o.ignorecase    = true                                 -- case insensitive searching
-    o.incsearch     = true                                 -- set incremental search, like modern browsers
-    o.smartcase     = true                                 -- case-sensitive if expresson contains a capital letter
-    o.lazyredraw    = false                                -- don't redraw while executing macros
-    o.magic         = true                                 -- Set magic on, comfortable for regex
+-- bells
+o.visualbell    = false                                -- disable flash screen
+o.errorbells    = false                                -- disable errors bells
 
-    -- bells
-    o.visualbell    = false                                -- disable flash screen
-    o.errorbells    = false                                -- disable errors bells
+-- appearance
+o.cursorcolumn  = false                                -- highlight current column
+o.cursorline    = false                                -- highligh current line
+o.laststatus    = 2                                    -- always show the status line - use 2 lines for the status bar
+o.number        = true                                 -- show line numbers
+o.showcmd       = true                                 -- display command writing
+o.showmode      = true                                 -- display vim mode
+o.showtabline   = 0                                    -- always show tabline
+o.title         = true                                 -- set terminal title
+o.wrap          = true                                 -- turn on line wrapping
+o.signcolumn    = 'yes'                                -- sign column always on
+o.foldenable    = false                                -- disable session fold
+o.foldmethod    = 'marker'                             -- use markers as to specify folds.
+o.termguicolors = true                                 -- enable 24 bit color support if supported
+o.guifont       = 'CaskaydiaCove NF:h16'               -- font for gui
 
-    -- appearance
-    o.cursorcolumn  = false                                -- highlight current column
-    o.cursorline    = false                                -- highligh current line
-    o.laststatus    = 2                                    -- always show the status line - use 2 lines for the status bar
-    o.number        = true                                 -- show line numbers
-    o.showcmd       = true                                 -- display command writing
-    o.showmode      = true                                 -- display vim mode
-    o.showtabline   = 0                                    -- always show tabline
-    o.title         = true                                 -- set terminal title
-    o.wrap          = true                                 -- turn on line wrapping
-    o.signcolumn    = 'yes'                                -- sign column always on
-    o.foldenable    = false                                -- disable session fold
-    o.foldmethod    = 'marker'                             -- use markers as to specify folds.
-    o.termguicolors = true                                 -- enable 24 bit color support if supported
-    o.guifont       = 'CaskaydiaCove NF:h16'               -- font for gui
+-- tab Control
+o.shiftround    = true                                 -- round indent to a multiple of 'shiftwidth'
+o.shiftwidth    = 4                                    -- number of spaces to use for indent and unindent
+o.smarttab      = true                                 -- tab respects 'tabstop', 'shiftwidth', and 'softtabstop'
+o.softtabstop   = 4                                    -- edit as if the tabs are 4 characters wide
+o.tabstop       = 4                                    -- the visible width of tabs
+o.expandtab     = true                                 -- expand tab to space
 
-    -- tab Control
-    o.shiftround    = true                                 -- round indent to a multiple of 'shiftwidth'
-    o.shiftwidth    = 4                                    -- number of spaces to use for indent and unindent
-    o.smarttab      = true                                 -- tab respects 'tabstop', 'shiftwidth', and 'softtabstop'
-    o.softtabstop   = 4                                    -- edit as if the tabs are 4 characters wide
-    o.tabstop       = 4                                    -- the visible width of tabs
-    o.expandtab     = true                                 -- expand tab to space
-end
+-- disable_distribution_plugins
+g.loaded_gzip              = 1
+g.loaded_tar               = 1
+g.loaded_tarPlugin         = 1
+g.loaded_zip               = 1
+g.loaded_zipPlugin         = 1
+g.loaded_getscript         = 1
+g.loaded_getscriptPlugin   = 1
+g.loaded_vimball           = 1
+g.loaded_vimballPlugin     = 1
+g.loaded_matchit           = 1
+g.loaded_matchparen        = 1
+g.loaded_2html_plugin      = 1
+g.loaded_logiPat           = 1
+g.loaded_rrhelper          = 1
+g.loaded_netrw             = 1
+g.loaded_netrwPlugin       = 1
+g.loaded_netrwSettings     = 1
+g.loaded_netrwFileHandlers = 1
 
-return options
+-- setup global map leader
+g.mapleader                = ","
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,126 +1,60 @@
-core.plugins = {
-    completion = require("completion"),
-    statusline = require("statusline"),
-    telescopes = require("telescopes"),
-    tsconfig = require("tsconfig"),
-}
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
-local plugins = core.plugins
-
-function plugins:load_packer()
-    if not self.packer then
-        vim.api.nvim_command("packadd packer.nvim")
-        self.packer = require("packer")
-
-        self.packer.init(
-            {
-                package_root = self.package_root,
-                compile_path = self.compile_path,
-                git = {clone_timeout = 600}
-            }
-        )
-
-        self.packer.reset()
-
-        self.packer.startup(
-            function()
-                use {"wbthomason/packer.nvim", opt = true}
-
-                -- colorscheme and semantic highlight
-                use {
-                    "glepnir/galaxyline.nvim",
-                    branch = "main",
-                    config = self.statusline.setup,
-                    requires = {"kyazdani42/nvim-web-devicons"}
-                }
-                use {
-                    "nvim-treesitter/nvim-treesitter",
-                    config = self.tsconfig.setup,
-                    requires = {"p00f/nvim-ts-rainbow"}
-                }
-
-                -- enhanced text edit
-                use {"editorconfig/editorconfig-vim"}
-                use {"sheerun/vim-polyglot"}
-                use {"fidian/hexmode", ft = {"bin", "img"}}
-                use {"mg979/vim-visual-multi"}
-
-                -- language specific
-                use {"rust-lang/rust.vim", ft = {"rust"}}
-
-                -- git related
-                use {"airblade/vim-gitgutter"}
-                use {"tpope/vim-fugitive"}
-
-                -- lspconfig
-                use {
-                    "neovim/nvim-lspconfig",
-                    ft = {"rust", "c", "cpp", "python"},
-                    config = self.completion.lspconfig
-                }
-
-                -- completion
-                use {
-                    "hrsh7th/nvim-compe",
-                    config = self.completion.compe,
-                    requires = {
-                        {
-                            "hrsh7th/vim-vsnip",
-                            config = function()
-                                vim.g.vsnip_snippet_dir = vim.fn.stdpath("config") .. "/snippets"
-                            end
-                        },
-                        {
-                            "onsails/lspkind-nvim",
-                            config = function()
-                                require "lspkind".init()
-                            end
-                        },
-                        {"rafamadriz/friendly-snippets"}
-                    }
-                }
-
-                -- navigation
-                use {
-                    "nvim-telescope/telescope.nvim",
-                    cmd = "Telescope",
-                    config = self.telescopes.setup,
-                    requires = {
-                        {"nvim-lua/popup.nvim"},
-                        {"nvim-lua/plenary.nvim"}
-                    }
-                }
-                use {
-                    "kyazdani42/nvim-tree.lua",
-                    requires = 'kyazdani42/nvim-web-devicons',
-                    config = function() require'nvim-tree'.setup {} end
-                }
-
-                -- utility
-                use {"voldikss/vim-floaterm", cmd = "FloatermNew"}
-            end
-        )
-    end
+if fn.empty(fn.glob(install_path)) > 0 then
+	fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	require("packer").sync()
 end
 
-function plugins:sync()
-    self:load_packer()
-    self.packer.sync()
-end
+return require("packer").startup(function(use)
+	-- Appearence
+	use({
+		"navarasu/onedark.nvim",
+		config = function()
+			require("onedark").load()
+		end,
+	})
+	use({
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup()
+		end,
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-function plugins:init()
-    local config_path = vim.fn.stdpath("config")
+	-- Navigation
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	})
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    self.install_path = config_path .. "/pack/packer/opt/packer.nvim"
-    self.package_root = config_path .. "/pack"
-    self.compile_path = config_path .. "/plugin/packer_compiled.vim"
+	-- File type specificed
+	use({ "rust-lang/rust.vim", ft = { "rust" } })
 
-    if not vim.loop.fs_stat(self.install_path) then
-        vim.api.nvim_command("!git clone https://github.com/wbthomason/packer.nvim " .. self.install_path)
-        self:sync()
-    else
-        self:load_packer()
-    end
-end
-
-return plugins
+	-- Advanced
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "rust" },
+			})
+		end,
+	})
+	use("editorconfig/editorconfig-vim")
+end)
