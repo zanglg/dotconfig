5f871a0ce4dc90cef40c3d4b3356baf8bd24e1ce
Zang Leigang <zang.lg@outlook.com>
2022-04-04 17:43:04 +0800

    enable incremental selection

--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -3,5 +3,14 @@ return function()
         ensure_installed = "maintained",
         rainbow = { enable = true },
         matchup = { enable = true },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "gnn",
+                node_incremental = "grn",
+                scope_incremental = "grc",
+                node_decremental = "grm",
+            },
+        },
     })
 end
