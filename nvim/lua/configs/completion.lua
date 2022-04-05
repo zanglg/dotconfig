2d699cf40d760bc4f4d6784d696afab8bb5bd2a5
Zang Leigang <zang.lg@outlook.com>
2022-04-05 11:54:32 +0800

    add cmp-path

--- nvim/lua/configs/completion.lua
+++ nvim/lua/configs/completion.lua
@@ -32,6 +32,7 @@ return function()
 		sources = cmp.config.sources({
 			{ name = "nvim_lsp" },
 			{ name = "luasnip" },
+			{ name = "path" },
 		}),
 	})
 
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -100,6 +100,7 @@ return require("packer").startup(function(use)
 	use({ "L3MON4D3/LuaSnip" })
 	use({ "saadparwaiz1/cmp_luasnip" })
 	use({ "rafamadriz/friendly-snippets" })
+	use({ "hrsh7th/cmp-path" })
 	use({ "hrsh7th/cmp-nvim-lsp" })
 	use({
 		"windwp/nvim-autopairs",
