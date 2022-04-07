d1447955c5f7e48d017fc15d488e484c4bec7eb1
Zang Leigang <zang.lg@outlook.com>
2022-04-07 15:09:15 +0800

    add some telescope key binding

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -15,8 +15,13 @@ return function()
 		["<C-l>"] = { "<cmd>lua require'smart-splits'.move_cursor_right()<cr>", "window right" },
 
 		---------- sidebar toggle ----------
-		["<leader>N"] = { "<cmd>NvimTreeToggle<cr>", "NvimTree" },
-		["<leader>S"] = { "<cmd>SymbolsOutline<cr>", "SymbolsOutline" },
+		["<leader>N"] = { "<cmd>NvimTreeToggle<cr>", "nvim-tree toggle" },
+		["<leader>S"] = { "<cmd>SymbolsOutline<cr>", "symbols-outline toggle" },
+
+		---------- telescope ----------
+		["<leader>ff"] = { "<cmd>lua require('telescope.builtin').find_files()<cr>", "find files" },
+		["<leader>fg"] = { "<cmd>lua require('telescope.builtin').live_grep()<cr>", "live grep" },
+		["<leader>fb"] = { "<cmd>lua require('telescope.builtin').buffers()<cr>", "find buffers" },
 
 		---------- lsp ----------
 		["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "declaration" },
@@ -26,5 +31,6 @@ return function()
 		["gr"] = { "<cmd>lua vim.lsp.buf.references()<cr>", "reference" },
 		["<leader>f"] = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "format" },
 		["<leader>rn"] = { "<cmd>lua vim.lsp.buf.rename()<cr>", "rename" },
+		["<leader>ca"] = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "code action" },
 	})
 end
