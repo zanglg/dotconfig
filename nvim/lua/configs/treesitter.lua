2a3321ecd2eaaf20fcacd01b25ea9a99904aeb13
Zang Leigang <zang.lg@outlook.com>
2022-04-13 10:03:01 +0800

    fix wrong file type for markdown

--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -1,6 +1,6 @@
 return function()
 	require("nvim-treesitter.configs").setup({
-		ensure_installed = { "rust", "c", "cpp", "python", "lua", "md" },
+		ensure_installed = { "rust", "c", "cpp", "python", "lua", "markdown" },
 		rainbow = { enable = true },
 		matchup = { enable = true },
 		highlight = { enable = true },
