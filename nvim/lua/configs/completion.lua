25adff5327aec9dc5b85bf1b217f227f0f782569
Zang Leigang <zang.lg@outlook.com>
2022-04-21 20:02:04 +0800

    switch completion item by <C-n> and <C-p>

--- nvim/lua/configs/completion.lua
+++ nvim/lua/configs/completion.lua
@@ -24,7 +24,7 @@ return function()
 		},
 
 		mapping = cmp.mapping.preset.insert({
-			["<Tab>"] = cmp.mapping(function(fallback)
+			["<C-n>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_next_item()
 				elseif luasnip.expand_or_jumpable() then
@@ -36,7 +36,7 @@ return function()
 				end
 			end, { "i", "s" }),
 
-			["<S-Tab>"] = cmp.mapping(function(fallback)
+			["<C-p>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_prev_item()
 				elseif luasnip.jumpable(-1) then
