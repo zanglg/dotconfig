6c0b2aac267b71f2ce528ab827f92b58875c680d
Zang Leigang <zang.lg@outlook.com>
2022-09-30 21:29:03 +0800

    git blame current line enabled default

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -175,7 +175,10 @@ local packages = function(use)
     use({
         "lewis6991/gitsigns.nvim",
         config = function()
-            require("gitsigns").setup()
+            require("gitsigns").setup({
+                current_line_blame = true,
+                current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d>, <abbrev_sha> - <summary>",
+            })
         end,
     })
 
