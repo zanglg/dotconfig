d581738864467a6da2afe639dcb23aca0f8181f7
Zang Leigang <zang.lg@outlook.com>
2022-10-21 21:48:22 +0800

    disable performance mode of neoscroll

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -82,9 +82,7 @@ local packages = function(use)
     use({
         "karb94/neoscroll.nvim",
         config = function()
-            require("neoscroll").setup({
-                performance_mode = true,
-            })
+            require("neoscroll").setup()
         end,
     })
     use({
