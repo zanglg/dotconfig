0a386bd6dac32cc970b34ef63f1a10adcbf80282
Zang Leigang <zang.lg@outlook.com>
2022-10-22 20:20:35 +0800

    build key maps from scratch

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -29,6 +29,7 @@ M.setup = function()
         ---------- diagnostic ----------
         d = {
             name = "diagnostic",
+
             n = { vim.diagnostic.goto_prev, "prev diagnostic" },
             p = { vim.diagnostic.goto_next, "next diagnostic" },
             e = { vim.diagnostic.open_float, "float diagnostic" },
@@ -42,95 +43,7 @@ M.setup = function()
         ---------- motion ----------
         s = { "<cmd>HopWord<cr>", "hop word" },
         f = { "<cmd>HopChar1<cr>", "hop char" },
-
-        ---------- modify ----------
-        m = {
-            name = "modify",
-
-            q = { "<cmd>quitall!<cr>", "quit all without save" },
-            W = { "<cmd>wq<cr>", "quit with save" },
-            w = { "<cmd>w<cr>", "save" },
-            d = { "<cmd>bdelete<cr>", "delete current buffer" },
-            l = { "<cmd>TSHighlightCapturesUnderCursor<cr>", "cursor highlight group" },
-        },
     }, { mode = "n" })
-
-    ---------- normal mode, leader key mapping ------------
-    wk.register({
-        ---------- code ----------
-        c = {
-            name = "code",
-
-            f = {
-                function()
-                    vim.lsp.buf.format({ async = true })
-                end,
-                "document format",
-            },
-            c = {
-                function()
-                    require("gitsigns").select_hunk()
-
-                    vim.lsp.buf.format({ async = true })
-                end,
-                "hunk format",
-            },
-            a = { vim.lsp.buf.code_action, "code action" },
-            r = { vim.lsp.buf.rename, "rename" },
-        },
-
-        ---------- symbols ----------
-        s = {
-            name = "symbols",
-
-            w = { require("telescope.builtin").lsp_workspace_symbols, "workspace symbols" },
-            s = { require("telescope.builtin").lsp_document_symbols, "document symbols" },
-        },
-
-        ---------- find ----------
-        f = {
-            name = "find",
-
-            f = { require("telescope.builtin").find_files, "find files" },
-            b = { require("telescope.builtin").buffers, "find buffers" },
-            g = { require("telescope.builtin").live_grep, "live grep" },
-            c = { require("telescope.builtin").grep_string, "search current word" },
-        },
-
-        ---------- git ----------
-        g = {
-            name = "git",
-
-            ---------- hunk ----------
-            n = { require("gitsigns").next_hunk, "next hunk" },
-            p = { require("gitsigns").prev_hunk, "prev hunk" },
-
-            K = { require("gitsigns").preview_hunk, "preview hunk" },
-            s = { require("gitsigns").select_hunk, "select hunk" },
-        },
-
-        ---------- toggle ----------
-        t = {
-            name = "toggle",
-
-            n = { "<cmd>NvimTreeToggle<cr>", "nvim-tree" },
-            s = { "<cmd>SymbolsOutline<cr>", "symbols-outline" },
-        },
-    }, { mode = "n", prefix = "<leader>" })
-
-    wk.register({
-        ---------- code ----------
-        c = {
-            name = "code",
-
-            f = {
-                function()
-                    vim.lsp.buf.format({ async = true })
-                end,
-                "range format",
-            },
-        },
-    }, { mode = "v", prefix = "<leader>" })
 end
 
 return M
