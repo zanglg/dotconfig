05232c53a0f96a4a44bfd6a04459fce66c1613e1
Zang Leigang <zang.lg@outlook.com>
2023-01-14 20:12:11 +0800

    add treesitter highlight support

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	event = "BufReadPre",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "c", "rust", "python", "lua", "vim", "help" },
+			highlight = { enable = true },
+		})
+	end,
+}
