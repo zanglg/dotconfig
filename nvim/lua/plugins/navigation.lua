a457f51bd9a4e41c25f66f6dfff821360816880b
Zang Leigang <zang.lg@outlook.com>
2023-01-15 21:43:53 +0800

    move telescope to navigation

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -1,5 +1,26 @@
 return {
     {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        cmd = { "Telescope" },
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    vimgrep_arguments = {
+                        "rg",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                        "--follow",
+                    },
+                },
+            })
+        end,
+    },
+    {
         "nvim-tree/nvim-tree.lua",
         dependencies = {
             "nvim-tree/nvim-web-devicons",
