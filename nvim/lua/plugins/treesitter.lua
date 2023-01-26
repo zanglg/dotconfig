489cc37ed27f80658de2a4788ced92f79e3bec01
Zang Leigang <zang.lg@outlook.com>
2023-01-26 20:20:14 +0800

    remove nvim-treesitter-context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,8 @@
 return {
     { "mfussenegger/nvim-treehopper" },
     {
-        "nvim-treesitter/nvim-treesitter-context",
-        event = "BufReadPre",
-        config = true,
-    },
-    {
         "nvim-treesitter/nvim-treesitter",
+        event = "BufReadPre",
         config = function()
             require("nvim-treesitter.configs").setup({
                 ensure_installed = { "c", "rust", "python", "lua", "vim", "help" },
