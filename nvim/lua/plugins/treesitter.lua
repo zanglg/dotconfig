6c383892c300461fef599926c78a2d23d525436c
Zang Leigang <zang.lg@outlook.com>
2023-01-27 21:58:35 +0800

    add more treesitter and treehopper keybinding

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,53 @@
 return {
-    { "mfussenegger/nvim-treehopper" },
+    {
+        "mfussenegger/nvim-treehopper",
+        keys = { { "m", mode = { "o", "x" } } },
+        config = function()
+            vim.cmd([[
+                omap     <silent> m :<C-U>lua require('tsht').nodes()<cr>
+                xnoremap <silent> m :lua require('tsht').nodes()<cr>
+            ]])
+        end,
+    },
     {
         "nvim-treesitter/nvim-treesitter",
         event = "BufReadPre",
+        build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "c", "rust", "python", "lua", "vim", "help" },
+                ensure_installed = {
+                    -- programming language
+                    "c",
+                    "rust",
+                    "python",
+                    -- markup lang
+                    "rst",
+                    "markdown",
+                    "markdown_inline",
+                    "dot",
+                    -- make
+                    "make",
+                    "bash",
+                    "ninja",
+                    -- config file
+                    "toml",
+                    "json",
+                    "yaml",
+                    -- neovim
+                    "lua",
+                    "vim",
+                    "help",
+                },
                 highlight = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<c-m>",
+                        node_incremental = "<c-m>",
+                        scope_incremental = "<nop>",
+                        node_decremental = "<c-n>",
+                    },
+                },
             })
         end,
     },
