561971824731ad4348b191bc3828dc06008b416c
Zang Leigang <zang.lg@outlook.com>
2023-02-03 23:45:03 +0800

    add null-ls

---
+++ nvim/lua/configs/format.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.format = function(client, buf)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
+        buffer = buf,
+        callback = function()
+            vim.lsp.buf.format({ async = true })
+        end,
+    })
+end
+
+return M
