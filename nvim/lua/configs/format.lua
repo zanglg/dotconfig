a8bde603a3f7f64065464abcfd08efe259bc7d3d
Zang Leigang <zang.lg@outlook.com>
2023-02-04 19:55:22 +0800

    format on sync

--- nvim/lua/configs/format.lua
+++ nvim/lua/configs/format.lua
@@ -5,7 +5,7 @@ M.format = function(client, buf)
         group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
         buffer = buf,
         callback = function()
-            vim.lsp.buf.format({ async = true })
+            vim.lsp.buf.format({ async = false })
         end,
     })
 end
