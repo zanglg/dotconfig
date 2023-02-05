43df9af179f921671728e5a64316f5ac67c42cee
Zang Leigang <zang.lg@outlook.com>
2023-02-05 20:35:25 +0800

    <c-n/p> used to jump between reference

--- nvim/lua/plugins/custom.lua
+++ nvim/lua/plugins/custom.lua
@@ -3,6 +3,9 @@ return {
         "configs/quickfix",
         dir = "configs",
         keys = {
+            { "<c-n>", "*<cmd>noh<cr>", desc = "Next Reference", mode = "n" },
+            { "<c-p>", "#<cmd>noh<cr>", desc = "Prev Reference", mode = "n" },
+
             { "q", require("configs/quickfix").toggle, desc = "Quickfix Toggle" },
         },
     },
