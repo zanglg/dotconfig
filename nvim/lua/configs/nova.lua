04b307039db7f9d05019ff61d444dfa93cb2fa20
Zang Leigang <zang.lg@outlook.com>
2023-02-23 22:02:41 +0800

    add nova dimmed color scheme

--- nvim/lua/configs/nova.lua
+++ nvim/lua/configs/nova.lua
@@ -45,6 +45,28 @@ local dark = {
     purple     = "#c678dd",
 }
 
+-- stylua: ignore
+local dimmed = {
+    foreground = "#9ca5b0",
+    comment    = "#5b6571",
+    separator  = "#393f47",
+    selection  = "#32373e",
+    stripline  = "#2b3036",
+    background = "#25292e",
+    popupmenu  = "#1e2125",
+
+    red        = "#e06c75",
+    brown      = "#93766c",
+    yellow     = "#e5c07b",
+    lime       = "#bac653",
+    green      = "#98c379",
+    teal       = "#1dc9ac",
+    cyan       = "#5edced",
+    blue       = "#61afef",
+    violet     = "#8c8cd9",
+    purple     = "#c678dd",
+}
+
 local colors = dark
 local default = {
     ColorColumn = { fg = colors.stripline },
