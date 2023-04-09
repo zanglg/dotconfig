3dbca563bbee1e956649a097df97e0c2b244abf8
Zang Leigang <zang.lg@outlook.com>
2023-04-10 01:47:22 +0800

    merge coding.lua to utility.lua

--- nvim/lua/plugins/utility.lua
+++ nvim/lua/plugins/utility.lua
@@ -1,5 +1,22 @@
 return {
     {
+        "numToStr/Comment.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("Comment").setup()
+        end,
+    },
+    {
+        "kevinhwang91/nvim-bqf",
+        ft = "qf",
+        cmd = "BqfToggle",
+        config = function()
+            require("bqf").setup({
+                auto_enable = false,
+            })
+        end,
+    },
+    {
         "ellisonleao/glow.nvim",
         cmd = "Glow",
         config = function()
