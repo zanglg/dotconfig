4bbdc2ac8e44e74ef91de9bea4452caedfad208c
Zang Leigang <zang.lg@outlook.com>
2023-06-16 23:18:39 +0800

    don't touch terminal's gui cursor

--- nvim/lua/configs/opts.lua
+++ nvim/lua/configs/opts.lua
@@ -40,7 +40,7 @@ vim.opt.signcolumn = "yes" -- sign column always on
 vim.opt.foldenable = false -- disable session fold
 vim.opt.foldmethod = "manual" -- folds are created manually
 vim.opt.termguicolors = true -- enable 24 bit color support if supported
-vim.opt.guicursor = "n-v-o-r-c-cr:hor25,i-ci-sm:ver100" -- override cursor shape
+vim.opt.guicursor = "" -- don't touch terminal's cursor shape
 vim.opt.scrolloff = 5 -- minimal number of screen lines to keep
 vim.opt.splitkeep = "screen" -- keep the text on the same screen line
 
