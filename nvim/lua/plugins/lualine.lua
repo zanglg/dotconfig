dc48ea7e86cbce8b46e2b8b5da15d66125c628c0
Zang Leigang <zang.lg@outlook.com>
2023-06-18 18:02:31 +0800

    add nvim-navic as one of lualine's component

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,22 +1,47 @@
-return {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "auto",
-                globalstatus = true,
-            },
-            sections = {
-                lualine_c = {
-                    {
-                        "filename",
-                        file_status = true,
-                        path = 1,
+local M = {
+    {
+        "SmiteshP/nvim-navic",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-navic").setup({
+                lsp = {
+                    auto_attach = true,
+                    highlight = true,
+                },
+            })
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "auto",
+                    globalstatus = true,
+                },
+                sections = {
+                    lualine_c = {
+                        {
+                            "filename",
+                            file_status = true,
+                            path = 1,
+                        },
+                        {
+                            "navic",
+                        },
                     },
                 },
-            },
-            extensions = { "quickfix", "nvim-tree", "symbols-outline" },
-        })
-    end,
+                extensions = { "quickfix", "nvim-tree", "symbols-outline" },
+            })
+        end,
+    },
 }
+
+return M
