f431d63ae004860252e6e23c07dc85277c829fd8
Zang Leigang <zang.lg@outlook.com>
2023-07-19 21:34:14 +0800

    auto highlight active window's cursor line

--- nvim/lua/configs/cmds.lua
+++ nvim/lua/configs/cmds.lua
@@ -1,4 +1,4 @@
-if vim.loop.os_uname().sysname == "Darwin" then
+if vim.loop.os_uname().sysname == "Darwin" and vim.fn.executable("im-select") == 1 then
     vim.api.nvim_create_autocmd({
         "VimEnter",
         "InsertLeave",
@@ -9,3 +9,19 @@ if vim.loop.os_uname().sysname == "Darwin" then
         group = vim.api.nvim_create_augroup("im-select", { clear = true }),
     })
 end
+
+vim.api.nvim_create_autocmd("WinEnter", {
+    group = vim.api.nvim_create_augroup("win-enter", { clear = true }),
+    callback = function()
+        vim.wo.cursorcolumn = true -- enable highlight current column
+        vim.wo.cursorline = true -- enable highligh current line
+    end,
+})
+
+vim.api.nvim_create_autocmd("WinLeave", {
+    group = vim.api.nvim_create_augroup("win-leave", { clear = true }),
+    callback = function()
+        vim.wo.cursorcolumn = false -- clear highlight current column
+        vim.wo.cursorline = false -- clear highligh current line
+    end,
+})
