e272a432009055695d500719b1e5303d31ee5be5
Zang Leigang <zang.lg@outlook.com>
2023-07-22 16:36:51 +0800

    add down and up in insert mode

--- nvim/lua/configs/maps.lua
+++ nvim/lua/configs/maps.lua
@@ -3,6 +3,8 @@ local keymap = vim.api.nvim_set_keymap
 
 keymap("i", "<c-l>", "<right>", opts)
 keymap("i", "<c-h>", "<left>", opts)
+keymap("i", "<c-j>", "<down>", opts)
+keymap("i", "<c-k>", "<up>", opts)
 
 keymap("n", "H", "^", opts)
 keymap("n", "L", "$", opts)
