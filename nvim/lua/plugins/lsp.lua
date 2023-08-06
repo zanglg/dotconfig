51774017baad4b3137ae9c93a8b3154d312b63d1
Zang Leigang <zang.lg@outlook.com>
2023-08-06 23:38:42 +0800

    remove request actions of lua_ls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -47,6 +47,9 @@ return {
             require("lspconfig")["lua_ls"].setup({
                 settings = {
                     Lua = {
+                        workspace = {
+                            checkThirdParty = false,
+                        },
                         completion = {
                             callSnippet = "Replace",
                         },
