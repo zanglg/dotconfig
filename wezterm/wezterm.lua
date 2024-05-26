d2c2a54925cf15e525e8d056968efb7956a4e186
Zang Leigang <zang.lg@outlook.com>
2024-05-26 21:54:08 +0800

    wezterm: basic config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "Batman"
+config.font = wezterm.font("MonaspiceAr Nerd Font")
+config.font_size = 14.0
+config.use_fancy_tab_bar = false
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
+return config
