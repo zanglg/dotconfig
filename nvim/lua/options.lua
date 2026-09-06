9fac239e8f0eac730d5d0ece3af230c977a3f031
Zang Leigang <zanglg@users.noreply.github.com>
2026-09-06 23:22:10 +0800

    nvim: enable native autocomplete

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -12,6 +12,8 @@ vim.opt.fileencodings = { "ucs-bom", "utf-8", "cp936", "gb18030", "latin1" }
 vim.opt.clipboard = "unnamedplus"
 vim.opt.whichwrap = "<,>,h,l,[,]"
 vim.opt.formatoptions = "tcqmMj"
+vim.opt.autocomplete = true
+vim.opt.complete = { ".^5", "w^5", "b^5", "o^10" }
 vim.opt.completeopt = { "menuone", "noselect", "popup" }
 vim.opt.mouse = ""
 
