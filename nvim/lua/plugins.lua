8ab23541902d52db1370777ca6064a27ac257de3
Zang Leigang <zanglg@users.noreply.github.com>
2026-09-06 23:36:19 +0800

    nvim: fail fast on core plugin setup errors

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,80 +1,62 @@
 require("nova").setup({ theme = "dark" })
 vim.cmd.colorscheme("nova")
 
-pcall(function()
-    require("lualine").setup({
-        options = {
-            theme = "nova",
-            globalstatus = true,
+require("lualine").setup({
+    options = {
+        theme = "nova",
+        globalstatus = true,
+    },
+    sections = {
+        lualine_c = {
+            { "filename", file_status = true, path = 1 },
         },
-        sections = {
-            lualine_c = {
-                { "filename", file_status = true, path = 1 },
+    },
+    extensions = { "quickfix", "nvim-tree" },
+})
+
+require("which-key").setup()
+
+require("noice").setup({
+    cmdline = {
+        view = "cmdline",
+    },
+})
+
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+
+telescope.setup({
+    pickers = {
+        buffers = {
+            mappings = {
+                i = { ["<C-d>"] = actions.delete_buffer },
             },
         },
-        extensions = { "quickfix", "nvim-tree" },
-    })
-end)
-
-pcall(function()
-    require("which-key").setup()
-end)
-
-pcall(function()
-    require("noice").setup({
-        cmdline = {
-            view = "cmdline",
+        grep_string = {
+            additional_args = { "--follow" },
         },
-    })
-end)
-
-pcall(function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-
-    telescope.setup({
-        pickers = {
-            buffers = {
-                mappings = {
-                    i = { ["<C-d>"] = actions.delete_buffer },
-                },
-            },
-            grep_string = {
-                additional_args = { "--follow" },
-            },
+    },
+    extensions = {
+        live_grep_args = {
+            additional_args = { "--follow" },
         },
-        extensions = {
-            live_grep_args = {
-                additional_args = { "--follow" },
-            },
-            zoxide = {},
-        },
-    })
+        zoxide = {},
+    },
+})
 
-    pcall(telescope.load_extension, "live_grep_args")
-    pcall(telescope.load_extension, "zoxide")
-end)
+pcall(telescope.load_extension, "live_grep_args")
+pcall(telescope.load_extension, "zoxide")
 
-pcall(function()
-    require("flash").setup({
-        modes = {
-            search = { enabled = false },
-            char = { keys = { "f", "F", "t", "T", ";" } },
-        },
-    })
-end)
+require("flash").setup({
+    modes = {
+        search = { enabled = false },
+        char = { keys = { "f", "F", "t", "T", ";" } },
+    },
+})
 
-pcall(function()
-    require("nvim-tree").setup()
-end)
-
-pcall(function()
-    require("outline").setup({})
-end)
-
-pcall(function()
-    require("nvim-autopairs").setup({})
-end)
+require("nvim-tree").setup()
+require("outline").setup({})
+require("nvim-autopairs").setup({})
 
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
@@ -93,13 +75,9 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
-pcall(function()
-    require("rainbow-delimiters.setup").setup({})
-end)
+require("rainbow-delimiters.setup").setup({})
 
-pcall(function()
-    require("gitsigns").setup({
-        current_line_blame = true,
-        current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d>, <abbrev_sha> - <summary>",
-    })
-end)
+require("gitsigns").setup({
+    current_line_blame = true,
+    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d>, <abbrev_sha> - <summary>",
+})
