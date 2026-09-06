c7f546b60abba0dccd64d6d5278777a5a8f6aaa1
Zang Leigang <zanglg@users.noreply.github.com>
2026-09-06 23:42:38 +0800

    nvim: simplify native autocomplete config

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -12,14 +12,13 @@ vim.opt.fileencodings = { "ucs-bom", "utf-8", "cp936", "gb18030", "latin1" }
 vim.opt.clipboard = "unnamedplus"
 vim.opt.whichwrap = "<,>,h,l,[,]"
 vim.opt.formatoptions = "tcqmMj"
-vim.opt.autocomplete = false
+vim.opt.autocomplete = true
 vim.opt.complete = { ".^5", "w^5", "b^5", "o^10" }
-vim.opt.completeopt = { "menuone", "popup", "preinsert" }
+vim.opt.completeopt = { "menuone", "popup" }
 vim.opt.mouse = ""
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.infercase = true
 
 vim.opt.laststatus = 3
 vim.opt.number = true
@@ -32,24 +31,6 @@ vim.opt.foldenable = false
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 5
 vim.opt.splitkeep = "screen"
-
-local autocomplete_group = vim.api.nvim_create_augroup("config-autocomplete", { clear = true })
-
-vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
-    group = autocomplete_group,
-    callback = function(args)
-        vim.api.nvim_set_option_value("autocomplete", false, { buf = args.buf })
-    end,
-})
-
-vim.api.nvim_create_autocmd("TextChangedI", {
-    group = autocomplete_group,
-    callback = function(args)
-        if not vim.api.nvim_get_option_value("autocomplete", { buf = args.buf }) then
-            vim.api.nvim_set_option_value("autocomplete", true, { buf = args.buf })
-        end
-    end,
-})
 
 if vim.fn.executable("rg") == 1 then
     vim.opt.grepprg = "rg --vimgrep --smart-case --follow --no-messages"
