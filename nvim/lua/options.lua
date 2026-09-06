4b2e891548ecaba2b1fbecbc8d6001392c996041
Zang Leigang <zanglg@users.noreply.github.com>
2026-09-06 23:34:01 +0800

    nvim: defer autocomplete until insert text changes

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -12,7 +12,7 @@ vim.opt.fileencodings = { "ucs-bom", "utf-8", "cp936", "gb18030", "latin1" }
 vim.opt.clipboard = "unnamedplus"
 vim.opt.whichwrap = "<,>,h,l,[,]"
 vim.opt.formatoptions = "tcqmMj"
-vim.opt.autocomplete = true
+vim.opt.autocomplete = false
 vim.opt.complete = { ".^5", "w^5", "b^5", "o^10" }
 vim.opt.completeopt = { "menuone", "popup", "preinsert" }
 vim.opt.mouse = ""
@@ -32,6 +32,24 @@ vim.opt.foldenable = false
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 5
 vim.opt.splitkeep = "screen"
+
+local autocomplete_group = vim.api.nvim_create_augroup("config-autocomplete", { clear = true })
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+    group = autocomplete_group,
+    callback = function(args)
+        vim.api.nvim_set_option_value("autocomplete", false, { buf = args.buf })
+    end,
+})
+
+vim.api.nvim_create_autocmd("TextChangedI", {
+    group = autocomplete_group,
+    callback = function(args)
+        if not vim.api.nvim_get_option_value("autocomplete", { buf = args.buf }) then
+            vim.api.nvim_set_option_value("autocomplete", true, { buf = args.buf })
+        end
+    end,
+})
 
 if vim.fn.executable("rg") == 1 then
     vim.opt.grepprg = "rg --vimgrep --smart-case --follow --no-messages"
